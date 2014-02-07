e501bb487e34f81cc804cc330a1e877bf4e53aef
tuffk <tuffkmulhall@gmail.com>
2014-02-06 22:42:11 -0600

    prueba 1

--- tarea busqueda en files/tarea busqueda en files/main.cpp
+++ tarea busqueda en files/tarea busqueda en files/main.cpp
@@ -9,14 +9,96 @@ en el archivo, imprima las líneas que contienen dicha frase con el número de lin
 correspondiente.
 */
 
-#include<cstdlib>
-#include<stdio.h>
-#include<iostream>
+#include <cstdlib>
+#include <sstream>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
 using namespace std;
 
-int main(){
 
-	
 
+int funcion(string c,ifstream &a){ 
+	int o=0,p=1;
+	string l; 
+
+	while(getline(a,l)){ 
+		if(l.find(c)!=string::npos){ 
+			cout<<"Cadena encontrada en la linea "<<p<<endl; 
+			o++;
+		} 
+		p++;
+	} 
+	a.close(); 
+	return o;
+} 
+
+bool CheckWord(char* filename, char* search)
+{
+
+
+
+    int offset; 
+    string line;
+    ifstream Myfile;
+    Myfile.open (filename);
+
+    if(Myfile.is_open())
+    {
+        while(!Myfile.eof())
+        {
+            getline(Myfile,line);
+            if ((offset = line.find(search, 0)) != string::npos) 
+            {
+             cout << "found '" << search << " \n\n"<< line  <<endl;
+             return true;
+            }
+            else
+            {
+
+                cout << "Not found \n\n";
+
+            }
+        }
+        Myfile.close();
+    }
+    else
+    cout<<"Unable to open this file."<<endl;
+
+    return false;
+}
+
+/*
+int main () 
+{
+
+    CheckWord("dictionary.txt", "need");
+
+    return 0;
+}
+*/
+int main() { 
+	 string c = "hola";
+
+	 CheckWord("pruebas.txt", "hola");
+	 cout <<"PEN"<<endl;
+
+
+FILE * pFile;
+   char buffer [100];
+
+   pFile = fopen ("pruebas.txt" , "r");
+   if (pFile == NULL) perror ("Error opening file");
+   else
+   {
+     while ( ! feof (pFile) )
+     {
+       if ( fgets (buffer , 100 , pFile) == NULL ) break;
+       fputs (buffer , stdout);
+	  
+     }
+     fclose (pFile);
+   }
 	return 0;
 }
